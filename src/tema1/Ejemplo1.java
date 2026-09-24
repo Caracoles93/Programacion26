c119d8dfd907f52316d606c5b8c8ad12d9e1c20e
angel_david <angelxperiam@gmail.com>
2026-09-24 21:06:36 +0200

    a

--- src/tema1/Ejemplo1.java
+++ src/tema1/Ejemplo1.java
@@ -2,7 +2,10 @@ package tema1;
 
 public class Ejemplo1 {
     static void main() {
+
         String nombre = "Ángel David";
         IO.println("Hola me llamo " + nombre);
+        IO.println("Estoy en casa");
+        IO.println("Estoy en casa otra vez");
     }
 }
